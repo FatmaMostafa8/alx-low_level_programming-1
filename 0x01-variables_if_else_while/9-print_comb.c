c3fd66719f91fc580e8872e7bf9e7b8749d8d2e7
Abdallah Mohamed <abdallahmohamed1120@gmail.com>
2023-02-17 04:59:50 -0800

    Done

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,25 +6,18 @@
  */
 int main(void)
 {
-	int num;
+	int i;
 
-	for (num = 0; num < 10; num++)
+	for (i = 0; i < 10; i++)
 	{
-		int next_num;
-
-		for (next_num = num + 1; next_num < 10; next_num++)
-		{
-			putchar(num + '0');
-			putchar(',');
-			putchar(' ');
-			putchar(next_num + '0');
-			if (num != 8 || next_num != 9)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
+	putchar(i + '0');
+	if (i != 9)
+	{
+		putchar(',');
+		putchar(' ');
 	}
+}
+
 	putchar('\n');
 	return (0);
 }
